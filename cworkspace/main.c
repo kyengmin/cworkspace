26d73831984b39653b5038c0a1e758568b42bc84
kyengmin <2342782@donga.ac.kr>
2023-03-25 22:20:11 +0900

    Initial Commit

---
+++ cworkspace/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cworkspace
+//
+//  Created by 김경민 on 2023/03/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
